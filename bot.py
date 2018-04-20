ce3269b5c3020c4b8742373b80080c621113684c
Maria Selezniova <maria.selezniova@phystech.edu>
2018-04-20 23:45:49 +0300

    Add proxy

--- bot.py
+++ bot.py
@@ -3,6 +3,8 @@ import telebot
 from telebot import apihelper
 
 bot = telebot.TeleBot(config.token)
+proxy = config.proxy()
+apihelper.proxy = {'https':'socks5://'+proxy.user+":"+proxy.password+"@"+proxy.address+":"+proxy.port}
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
--- config.py
+++ config.py
@@ -1,1 +1,9 @@
 token = ''
+
+class proxy():
+    def __init__(self, address = '', port = '', user = '', password = ''):
+        self.address = address
+        self.port = port
+        self.user = user
+        self.password = password
+
